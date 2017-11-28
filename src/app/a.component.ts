56143984fd26718799726dac1cd7b2170eb9afd8
maksym.koretskyi <maksym.koretskiy@globallogic.com>
2017-11-28 12:42:25 +0200

    Change detection is skipped for AComponent with OnPush

--- src/app/a.component.ts
+++ src/app/a.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
     selector: 'a-comp',
     template: `
         <div>I am A component, and here is value from my parent: "{{i}}"</div>
-    `
+        <b-comp></b-comp>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AComponent {
     @Input() i;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,6 @@ export class AppComponent {
     v = `value from parent`;
 
     fn() {
+        this.v = 'updated';
     }
 }
