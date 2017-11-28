5c484846580f433d932bffadb38b6d55c5259d33
maksym.koretskyi <maksym.koretskiy@globallogic.com>
2017-11-28 13:11:33 +0200

    Manual control of change detector: attach & detach

--- src/app/a.component.ts
+++ src/app/a.component.ts
@@ -1,24 +1,26 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
-import 'rxjs/add/operator/map';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
     selector: 'a-comp',
     template: `
-        <div>I am A component, and here is value from my parent: "{{i | async}}"</div>
-        <button>Update input binding inside</button>
+        <div>I am A component, and this is a demo of attaching/detaching change detector</div>
+        <button (click)="detach()">Detach</button>
+        <button (click)="attach()">Attach</button>
         <b-comp></b-comp>
-    `,
-    changeDetection: ChangeDetectionStrategy.OnPush
+    `
 })
 export class AComponent {
-    s = new Subject();
-    @Input() i = this.s.map(v => v);
 
-    constructor() {
-        setTimeout(() => {
-            console.log('updating AComponent `i` property from setTimeout through subject');
-            this.s.next('updated from Subject');
-        }, 2000);
+    constructor(private cd: ChangeDetectorRef) {
+    }
+
+    attach() {
+        console.log('attaching');
+        this.cd.reattach();
+    }
+
+    detach() {
+        console.log('detaching');
+        this.cd.detach();
     }
 }
