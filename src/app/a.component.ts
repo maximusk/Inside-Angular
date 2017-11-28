49324195a6f88f8d4b4622fb1e10b6037f891a4d
maksym.koretskyi <maksym.koretskiy@globallogic.com>
2017-11-28 13:04:48 +0200

    Change detection is triggered when updating input property through RxJS subject using async pipe

--- src/app/a.component.ts
+++ src/app/a.component.ts
@@ -1,24 +1,21 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/map';
 
 @Component({
     selector: 'a-comp',
     template: `
-        <div>I am A component, and here is value from my parent: "{{i}}"</div>
-        <button (click)="fn()">Update input binding inside</button>
+        <div>I am A component, and here is value from my parent: "{{i | async}}"</div>
+        <button>Update input binding inside</button>
         <b-comp></b-comp>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AComponent {
     s = new Subject();
-    @Input() i;
+    @Input() i = this.s.map(v => v);
 
     constructor() {
-        this.s.subscribe((v) => {
-            this.i = v;
-        });
-
         setTimeout(() => {
             console.log('updating AComponent `i` property from setTimeout through subject');
             this.s.next('updated from Subject');
